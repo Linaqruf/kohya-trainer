1c5f9e715573b955faf59a25de3574758723f522
Linaqruf <50163983+Linaqruf@users.noreply.github.com>
2023-01-17 19:16:03 +0700

    fix: using List instead of list for py3.8 support

--- networks/lora.py
+++ networks/lora.py
@@ -6,7 +6,7 @@
 import math
 import os
 import torch
-
+from typing import List
 
 class LoRAModule(torch.nn.Module):
   """
@@ -63,7 +63,7 @@ class LoRANetwork(torch.nn.Module):
     self.lora_dim = lora_dim
 
     # create module instances
-    def create_modules(prefix, root_module: torch.nn.Module, target_replace_modules) -> list[LoRAModule]:
+    def create_modules(prefix, root_module: torch.nn.Module, target_replace_modules) -> List[LoRAModule]:
       loras = []
       for name, module in root_module.named_modules():
         if module.__class__.__name__ in target_replace_modules:
